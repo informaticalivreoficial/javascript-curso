bd68edd85d9d139549995a22ad0faf677bd9706e
Renato Montanari <renatomontanari@ymail.com>
2021-09-27 10:19:11 -0300

    Forms created

---
+++ assets/js/dom.js
@@ -0,0 +1,13 @@
+
+
+//console.log(document.title = 'Javascript Dom');
+
+//document.write('Inserindo texto na página');
+
+
+//document.getElementById('teste').innerHTML = '<p>texto novo</p>';
+//document.getElementById('teste').innerText = 'texto novo';
+
+// setTimeout(function () {
+//     document.getElementById('teste').innerHTML = 'texto novo';
+// }, 3000);
